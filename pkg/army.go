c6ea2b79708987de8ce2d96af06d649e7b7ca503
Gilad Trachtenberg <90507859+GiladTrachtenberg@users.noreply.github.com>
2023-10-05 15:24:26 +0300

    Update army.go
    implementing attack logic - each soldier in the attacking army will pick a random enemy soldier to attack

--- pkg/army.go
+++ pkg/army.go
@@ -10,5 +10,33 @@ func (a *Army) AddSoldier(s Soldier) {
 }
 
 func (a *Army) Attack(enemy *Army) {
-	// Implement attack logic
+	// Seed the random number generator
+	rand.Seed(time.Now().UnixNano())
+
+	for _, attackingSoldier := range a.Soldiers {
+		if attackingSoldier.Health <= 0 {
+			continue // Skip if this soldier is defeated
+		}
+
+		// Pick a random enemy soldier
+		enemyIndex := rand.Intn(len(enemy.Soldiers))
+		targetSoldier := &enemy.Soldiers[enemyIndex]
+
+		// Skip if the target enemy soldier is already defeated
+		if targetSoldier.Health <= 0 {
+			continue
+		}
+
+		// Calculate damage (simplified)
+		damage := attackingSoldier.Attack - targetSoldier.Defence
+		if damage < 0 {
+			damage = 0
+		}
+
+		// Inflict damage
+		targetSoldier.Health -= damage
+		if targetSoldier.Health < 0 {
+			targetSoldier.Health = 0
+		}
+	}
 }
